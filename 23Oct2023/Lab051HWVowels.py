d9493ff2c69ce5544341ff1d336215f54abbd2f1
cs4193 <shetty.chetan4.cs@gmail.com>
2023-10-23 14:31:44 +0530

    added 13Oct work

--- 23Oct2023/Lab051HWVowels.py
+++ 23Oct2023/Lab051HWVowels.py
@@ -4,3 +4,4 @@ for char in str:
     if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
         count +=1
 print(f"{str} has {count} vowels and {len(str)-count} consonants")
+
